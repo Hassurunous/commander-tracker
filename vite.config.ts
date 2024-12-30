8b32dfee55de20bd6c8eb196ae8dd4fc4c84b98c
Michael Loubier <hassurunous@gmail.com>
2024-12-30 11:10:59 -0800

    Add service worker to support offline functionality going forward.

--- vite.config.ts
+++ vite.config.ts
@@ -10,22 +10,51 @@ export default defineConfig({
             manifest: {
                 name: 'Commander Life Tracker',
                 short_name: 'CommanderTracker',
-                description: 'Track life totals and other info for Commander games.',
+                description: 'Track life totals and counters for Commander games.',
                 theme_color: '#4CAF50',
                 icons: [
                     {
-                        src: 'assets/icon-192x192.png',
+                        src: '/icon-192x192.png',
                         sizes: '192x192',
                         type: 'image/png',
                     },
                     {
-                        src: 'assets/icon-512x512.png',
+                        src: '/icon-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
+                    },
+                ],
+            },
+            workbox: {
+                runtimeCaching: [
+                    {
+                        urlPattern: ({ request }) => request.destination === 'document',
+                        handler: 'NetworkFirst',
+                        options: {
+                            cacheName: 'html-cache',
+                        },
+                    },
+                    {
+                        urlPattern: ({ request }) =>
+                            ['style', 'script', 'worker'].includes(request.destination),
+                        handler: 'StaleWhileRevalidate',
+                        options: {
+                            cacheName: 'assets-cache',
+                        },
+                    },
+                    {
+                        urlPattern: ({ request }) => request.destination === 'image',
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'image-cache',
+                            expiration: {
+                                maxEntries: 50,
+                                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                            },
+                        },
                     },
                 ],
             },
         }),
     ],
 });
-
